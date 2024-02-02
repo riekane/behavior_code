646f0c980b2f91b2e5db8a496201e02f3b7267c5
Szzzesan <98128431+Szzzesan@users.noreply.github.com>
2024-02-02 12:12:12 -0500

    added generic version of user info

---
+++ stand_alone/user_info_generic.py
@@ -0,0 +1,25 @@
+import os
+
+"""
+Copy this file to a new one with the name 'user_info.py' in the same folder location as this one. Replace all BOLD_CASE
+variables with your own. GOOD LUCK!
+"""
+
+
+def get_user_info():
+    info_dict = {
+        'initials': 'USER_INITIALS',
+        'mouse_buttons': [['MOUSE_NAME', 'MOUSE_NAME', 'MOUSE_NAME'],
+                          ['MOUSE_NAME', 'MOUSE_NAME', 'MOUSE_NAME'],
+                          ['MOUSE_NAME', 'MOUSE_NAME', 'MOUSE_NAME']],
+        'mouse_assignments': {
+            'MOUSE_NAME': 'TASK_NAME',
+            'testmouse': 'TASK_NAME',
+        },
+        'desktop_ip': 'PUT_IP_ADDRESS_HERE',
+        'desktop_user': 'PUT_USERNAME_HERE',
+        'desktop_password': 'PUT_PASSWORD_HERE',
+        'desktop_user_root': os.path.join('C:/', 'PATH', 'TO', 'ROOT'),
+        'desktop_save_path': os.path.join('PATH', 'TO', 'DATA', 'FOLDER'),
+    }
+    return info_dict
